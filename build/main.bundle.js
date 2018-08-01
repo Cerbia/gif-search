f976e1cbbdcde85c1c921b3be968d34ab98d7d28
Bernadetta Wąsowska <bernadetta.wasowska@gmail.com>
2018-08-01 21:13:11 +0200

    Gif search feature

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -1,118 +1,159 @@
-//nazwę klasy | propsy czyli właściwości danego elementu (wejścia) |dzieci elementu, czyli to co ma się znaleźć wewnątrz. Może to być zarówno tekst jak i kolejny ReactElement
-var contacts = [{
-  id: 1,
-  firstName: 'Jan',
-  lastName: 'Nowak',
-  email: 'jan.nowak@example.com',
-  photo: 'https://i2.wp.com/entertainmentmesh.com/wp-content/uploads/2017/12/Nyan-Cat-meme-2.jpg?w=600'
-}, {
-  id: 2,
-  firstName: 'Adam',
-  lastName: 'Kowalski',
-  email: 'adam.kowalski@example.com',
-  photo: 'http://www.humorandmemes.com/wp-content/uploads/2016/07/13516159_1064012330360472_3071202375964824008_n.jpg'
-}, {
-  id: 3,
-  firstName: 'Zbigniew',
-  lastName: 'Koziol',
-  email: 'zbigniew.koziol@example.com',
-  photo: 'https://skinnyms.com/wp-content/uploads/2015/03/9-Best-Grumpy-Cat-Memes-1.jpg'
-}];
-var contactForm = {
-  firstName: '',
-  lastName: '',
-  email: '',
-  photo: ''
-};
+var GIPHY_API_URL = 'https://api.giphy.com';
+var GIPHY_PUB_KEY = 'BujhrGahc98VjRSZGaLYReLLXXEJIbYe';
 
 var App = React.createClass({
-  displayName: 'App',
+    displayName: 'App',
 
-  render: function () {
-    return React.createElement(
-      'div',
-      { className: 'app' },
-      React.createElement(ContactForm, { contactForm: contactForm }),
-      React.createElement(Contacts, { items: contacts })
-    );
-  }
-});
-var Contact = React.createClass({
-	displayName: 'Contact',
 
-	propTypes: {
-		url: React.PropTypes.string.isRequired,
-		name: React.PropTypes.string.isRequired,
-		surname: React.PropTypes.string.isRequired,
-		email: React.PropTypes.string.isRequired
-	},
-	render: function () {
-		return React.createElement(
-			'div',
-			{ className: 'contactItem' },
-			React.createElement('img', { className: 'contactImage', src: this.props.url }),
-			React.createElement(
-				'div',
-				{ className: 'contactData' },
-				React.createElement(
-					'p',
-					{ className: 'contactName' },
-					'Name: ',
-					this.props.name
-				),
-				React.createElement(
-					'p',
-					{ className: 'contactSurname' },
-					'Surname: ',
-					this.props.surname
-				),
-				React.createElement(
-					'a',
-					{ className: 'contactEmail', href: 'mailto:' + this.props.email },
-					this.props.email
-				)
-			)
-		);
-	}
-});
-var ContactForm = React.createClass({
-	displayName: 'ContactForm',
-
-	propTypes: {
-		contactForm: React.PropTypes.object.isRequired
-	},
-	render: function (argument) {
-		return React.createElement(
-			'form',
-			{ className: 'contactForm' },
-			React.createElement('input', { type: 'text', placeholder: 'first name', value: this.props.contactForm.firstName }),
-			React.createElement('input', { type: 'text', placeholder: 'last name', value: this.props.contactForm.firstName }),
-			React.createElement('input', { type: 'text', placeholder: 'email', value: this.props.contactForm.email }),
-			React.createElement(
-				'button',
-				{ type: 'submit' },
-				'Add Contact'
-			)
-		);
-	}
-
-});
-var Contacts = React.createClass({
-    displayName: 'Contacts',
-
-    propTypes: {
-        items: React.PropTypes.array.isRequired
+    getInitialState: function () {
+        return {
+            loading: false,
+            searchingText: '',
+            gif: {}
+        };
     },
-    render: function () {
-        var contacts = this.props.items.map(function (item) {
-            return React.createElement('li', { key: item.id }, React.createElement(Contact, { url: item.photo, name: item.firstName, surname: item.lastName, email: item.email }));
+
+    handleSearch: function (searchingText) {
+        this.setState({
+            loading: true
         });
+        this.getGif(searchingText, function (gif) {
+            this.setState({
+                loading: false,
+                gif: gif,
+                searchingText: searchingText
+            });
+        }.bind(this));
+    },
+
+    getGif: function (searchingText, callback) {
+        var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                var data = JSON.parse(xhr.responseText).data;
+                var gif = {
+                    url: data.fixed_width_downsampled_url,
+                    sourceUrl: data.url
+                };
+                callback(gif);
+            }
+        };
+        xhr.send();
+    },
+
+    render: function () {
+        var styles = {
+            margin: '0 auto',
+            textAlign: 'center',
+            width: '90%'
+        };
+
         return React.createElement(
-            'ul',
-            null,
-            contacts
+            'div',
+            { style: styles },
+            React.createElement(
+                'h1',
+                null,
+                'Wyszukiwarka GIFow!'
+            ),
+            React.createElement(
+                'p',
+                null,
+                'Znajd\u017A gifa na ',
+                React.createElement(
+                    'a',
+                    { href: 'http://giphy.com' },
+                    'giphy'
+                ),
+                '. Naciskaj enter, aby pobra\u0107 kolejne gify.'
+            ),
+            React.createElement(Search, {
+                onSearch: this.handleSearch
+            }),
+            React.createElement(Gif, {
+                loading: this.state.loading,
+                url: this.state.gif.url,
+                sourceUrl: this.state.gif.sourceUrl
+            })
         );
     }
+});
+var GIPHY_LOADING_URL = 'http://www.ifmo.ru/images/loader.gif';
+
+var styles = {
+	minHeight: '310px',
+	margin: '0.5em'
+};
+
+var Gif = React.createClass({
+	displayName: 'Gif',
+
+
+	getUrl: function () {
+		return this.props.sourceUrl || GIPHY_LOADING_URL;
+	},
+
+	render: function () {
+
+		var url = this.props.loading ? GIPHY_LOADING_URL : this.props.url;
+
+		return React.createElement(
+			'div',
+			{ style: styles },
+			React.createElement(
+				'a',
+				{ href: this.getUrl(), title: 'view this on giphy', target: 'new' },
+				React.createElement('img', { id: 'gif', src: url, style: { width: '100%', maxWidth: '350px' } })
+			)
+		);
+	}
+});
+var Search = React.createClass({
+  displayName: 'Search',
+
+
+  getInitialState() {
+    return {
+      searchingText: ''
+    };
+  },
+
+  handleChange: function (event) {
+    var searchingText = event.target.value;
+    this.setState({
+      searchingText: searchingText
+    });
+
+    if (searchingText.length > 2) {
+      this.props.onSearch(searchingText);
+    }
+  },
+
+  handleKeyUp: function (event) {
+    if (event.keyCode == 13) {
+      this.props.onSearch(this.state.searchingText);
+    }
+  },
+
+  render: function () {
+
+    var styles = {
+      fontSize: '1.5em',
+      width: '90%',
+      maxWidth: '350px'
+    };
+
+    return React.createElement('input', { type: 'text',
+      onChange: this.handleChange,
+      onKeyUp: this.handleKeyUp,
+      placeholder: 'Tutaj wpisz wyszukiwan\u0105 fraz\u0119',
+      style: styles,
+      value: this.state.searchTerm
+    });
+  }
+
 });
 var app = React.createElement(App, null); //React.createElement(App);
 ReactDOM.render(app, document.getElementById('app'));
--- components/App.js
+++ components/App.js
@@ -1,39 +1,65 @@
-//nazwę klasy | propsy czyli właściwości danego elementu (wejścia) |dzieci elementu, czyli to co ma się znaleźć wewnątrz. Może to być zarówno tekst jak i kolejny ReactElement
-var contacts = [
-  {
-    id: 1,
-    firstName: 'Jan',
-    lastName: 'Nowak',
-    email: 'jan.nowak@example.com',
-    photo: 'https://i2.wp.com/entertainmentmesh.com/wp-content/uploads/2017/12/Nyan-Cat-meme-2.jpg?w=600'
-  },
-  {
-    id: 2,
-    firstName: 'Adam',
-    lastName: 'Kowalski',
-    email: 'adam.kowalski@example.com',
-    photo: 'http://www.humorandmemes.com/wp-content/uploads/2016/07/13516159_1064012330360472_3071202375964824008_n.jpg'
-  },
-  {
-    id: 3,
-    firstName: 'Zbigniew',
-    lastName: 'Koziol',
-    email: 'zbigniew.koziol@example.com',
-    photo: 'https://skinnyms.com/wp-content/uploads/2015/03/9-Best-Grumpy-Cat-Memes-1.jpg'
-  }
-];
-var contactForm = {
-  firstName: '',
-  lastName: '',
-  email: '',
-  photo: ''
-};
+var GIPHY_API_URL = 'https://api.giphy.com';
+var GIPHY_PUB_KEY = 'BujhrGahc98VjRSZGaLYReLLXXEJIbYe';
 
 var App = React.createClass({
-	render: function() {
-		return (<div className={'app'}>
-        <ContactForm contactForm={contactForm}></ContactForm>
-        <Contacts items={contacts}></Contacts>
-      </div>);
-	}
+
+    getInitialState: function() {
+        return {
+            loading: false,
+            searchingText: '',
+            gif: {}
+        };
+    },
+
+    handleSearch: function(searchingText) {
+        this.setState({
+            loading: true
+        });
+        this.getGif(searchingText, function(gif) {
+            this.setState({
+                loading: false,
+                gif: gif,
+                searchingText: searchingText
+            });
+        }.bind(this));
+    },
+
+    getGif: function(searchingText, callback) {
+        var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                var data = JSON.parse(xhr.responseText).data;
+                var gif = {
+                    url: data.fixed_width_downsampled_url,
+                    sourceUrl: data.url
+                };
+                callback(gif);
+            }
+        }
+        xhr.send();
+    },
+
+
+    render: function() {
+        var styles = {
+            margin: '0 auto',
+            textAlign: 'center',
+            width: '90%'
+        };
+
+        return (<div style={styles}>
+            <h1>Wyszukiwarka GIFow!</h1>
+            <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+            <Search 
+                onSearch={this.handleSearch}
+            />
+            <Gif 
+                loading={this.state.loading}
+                url={this.state.gif.url}
+                sourceUrl={this.state.gif.sourceUrl}
+            />
+        </div>);
+    }
 });
